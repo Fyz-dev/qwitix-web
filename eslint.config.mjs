5f7b580a736366145ce28e222b090dac22642a4f
Fyz-dev <vladbutek@gmail.com>
2025-04-26 20:40:11 +0300

    chore(libs and eslint)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import eslint from '@eslint/js';
 import pluginNext from '@next/eslint-plugin-next';
+import pluginQuery from '@tanstack/eslint-plugin-query';
 import tsParser from '@typescript-eslint/parser';
 import prettier from 'eslint-config-prettier';
 import _import from 'eslint-plugin-import';
@@ -7,6 +8,9 @@ import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config} */
 export default tseslint.config(
+  // {
+  //   ...pluginQuery.configs['flat/recommended'],
+  // },
   {
     ignores: ['**/.next/**', '**/node_modules/**', '**/.turbo/**'],
     files: [
@@ -21,6 +25,7 @@ export default tseslint.config(
       eslint.configs.recommended,
       ...tseslint.configs.strict,
       ...tseslint.configs.stylistic,
+      ...pluginQuery.configs['flat/recommended'],
     ],
     plugins: {
       import: _import,
