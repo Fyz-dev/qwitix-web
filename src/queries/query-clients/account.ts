f2bc8b2d7f6d144929cf989dda1438279ed1d3f0
Fyz-dev <vladbutek@gmail.com>
2025-05-04 17:37:31 +0300

    fix: log

--- src/queries/query-clients/account.ts
+++ src/queries/query-clients/account.ts
@@ -1,8 +1,6 @@
 import { Account } from '@/gen/Account';
 
 export const accountQueryClient = (accessToken?: string) => {
-  console.log('accountQueryClient', accessToken);
-
   return new Account({
     baseURL: process.env.NEXT_PUBLIC_BASE_API_URL,
     headers: {
