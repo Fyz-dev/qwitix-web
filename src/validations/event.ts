e4509bf0bdd212001f2825de1249a0278f5b4e89
Fyz-dev <vladbutek@gmail.com>
2025-05-04 16:34:38 +0300

    feat: zod

---
+++ src/validations/event.ts
@@ -0,0 +1,20 @@
+import z from 'zod';
+
+export const eventSchema = z.object({
+  title: z
+    .string()
+    .nonempty('Title cannot be empty.')
+    .max(250, 'Title cannot exceed 250 characters.'),
+
+  description: z
+    .string()
+    .max(10000, 'Description cannot exceed 10000 characters.')
+    .optional(),
+
+  category: z
+    .string()
+    .nonempty('Category cannot be empty.')
+    .max(100, 'Category cannot exceed 100 characters.'),
+});
+
+export type EventSchemaType = z.infer<typeof eventSchema>;
