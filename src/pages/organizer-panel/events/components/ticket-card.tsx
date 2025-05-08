70dad18d28ee92d0c73bdea1a717ffd38e43fa05
Fyz-dev <vladbutek@gmail.com>
2025-05-08 14:21:06 +0300

    feat: ticket card

--- src/pages/organizer-panel/events/components/ticket-card.tsx
+++ src/pages/organizer-panel/events/components/ticket-card.tsx
@@ -1,5 +1,9 @@
+import { Edit } from 'lucide-react';
 import { FC } from 'react';
 
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import { Card } from '@/components/ui/card';
 import { ResponseTicketDTO } from '@/gen/data-contracts';
 
 interface TicketCardProps {
@@ -7,7 +11,35 @@ interface TicketCardProps {
 }
 
 const TicketCard: FC<TicketCardProps> = ({ ticket }) => {
-  return <div>{ticket.name}</div>;
+  return (
+    <Card className="flex-row items-center justify-between px-3 py-2">
+      <div className="flex flex-col gap-1">
+        <span className="text-sm">
+          {ticket.name} {`(0/${ticket.quantity})`}
+        </span>
+
+        <span className="text-muted-foreground text-xs">
+          You get: ${ticket.price}
+        </span>
+
+        <Badge asChild className="items-center" variant="secondary">
+          <div>
+            <div className="mt-0.5 size-3 rounded-full bg-[#00F0B3]" />
+            Sales end before the event
+          </div>
+        </Badge>
+      </div>
+
+      <Button
+        className="text-muted-foreground"
+        type="button"
+        variant="ghost"
+        size="icon"
+      >
+        <Edit />
+      </Button>
+    </Card>
+  );
 };
 
 export default TicketCard;
